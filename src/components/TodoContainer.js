4b65eed45ff827acb91b21e5a997036338c078eb
TemesgenA <temesgen.ayneka@gmail.com>
2022-11-03 13:27:11 +0300

    Dynamic CRUD Ops

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -5,6 +5,57 @@ import Header from './Header';
 
 const TodoContainer = () => {
   const [todos, setTodos] = useState(getInitialTodos())
+  useEffect(() => {
+    // storing todos items
+    const temp = JSON.stringify(todos)
+    localStorage.setItem("todos", temp)
+  }, [todos])
+
+  function getInitialTodos() {
+    // getting stored items
+    const temp = localStorage.getItem("todos")
+    const savedTodos = JSON.parse(temp)
+    return savedTodos || [] 
+  }
+ 
+  const handleChange = id => {
+    setTodos(prevState => prevState.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo, completed: !todo.completed
+        }
+      }
+      return todo
+    }))
+  }
+
+  const delTodo = id => {
+    setTodos([
+      ...todos.filter(todo => {
+        return todo.id !== id
+      }),
+    ])
+  };
+
+  const addTodoItem = title => {
+    const newTodo = {    
+      id: uuidv4(),    
+      title: title,    
+      completed: false  
+    };
+    setTodos([...todos, newTodo])
+  };
+
+  const setUpdate = (updatedTitle, id) => {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          todo.title = updatedTitle
+        }
+        return todo
+      })
+    )
+  }
 
   return (
     <div>
